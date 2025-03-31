4f61204085918f44a01b390e733eb583ce771dd7
AtomicRTX <dawid.kubacki54@gmail.com>
2025-03-31 15:04:50 +0200

    Add Hotjar

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,25 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if user can navigate slides using next and prev buttons', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-slide-active').then(($firstSlide) => {
+      const firstSlideText = $firstSlide.text();
+      cy.get('.swiper-button-next').click();
+      cy.wait(1000);
+      cy.get('.swiper-slide-active').should(($newSlide) => {
+        expect($newSlide.text()).not.to.eq(firstSlideText);
+      });
+    });
+    cy.get('.swiper-slide-active').then(($secondSlide) => {
+      const secondSlideText = $secondSlide.text();
+      cy.get('.swiper-button-prev').click();
+      cy.wait(1000);
+      cy.get('.swiper-slide-active').should(($prevSlide) => {
+        expect($prevSlide.text()).not.to.eq(secondSlideText);
+      });
+    });
+  });
+});
